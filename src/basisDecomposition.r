cfde272f6e5eeb261761a04f5acb918edaccb662
Wenzhao Xu <xuwz.thu@gmail.com>
2017-08-04 21:14:35 -0500

    reorganize NMF and add NMF method

--- src/basisDecomposition.r
+++ src/basisDecomposition.r
@@ -29,16 +29,19 @@ NMF_scale <- function(NMFRes, center = "basis"){
 
 	for(i in 1:ncol(W)){
 		if(center == "basis"){
+			print("conducting NMF and scale basis max to 12")
 			alpha <- 12 / max(W[,i])
 			W[,i] <- W[,i]*alpha
 			H[i,] <- H[i,]/alpha
 		}else if(center == "basis2"){
+			print("conducting NMF and scale basis mean to 6")
 			alpha <- 6 / mean(W[,i])
 			W[,i] <- W[,i]*alpha
 			H[i,] <- H[i,]/alpha
 		}
 
 		else if(center == "coef"){
+			print("conducting NMF and scale max coefficients to 1")
 			alpha <- 1 / max(H[i,])
 			# alpha <- 1 / max(H)
 			H[i,] <- H[i,] * alpha
@@ -48,12 +51,20 @@ NMF_scale <- function(NMFRes, center = "basis"){
 	return(list(basis = W, coef = H))
 }
 
-NMF_basis <- function(DOdata, r){
+NMF_basis <- function(DOdata, r, ...){
+	method <- list(...)$method
+
 	require(NMF)
 	DOdata <- as.matrix(DOdata)
 
-	print("conducting NMF and scale max basis to 15")
-	nmfRes <- nmf(DOdata, r, nrun = 60)
+	if(is.null(method)){
+		print("using default NMF fitting method")
+		nmfRes <- nmf(DOdata, r, nrun = 60)
+	}else{
+		print(paste0("using ",method, " fitting gmethod"))
+		nmfRes <- nmf(DOdata, r, nrun = 60, method = method, beta = 0.1)
+	}
+	
 
 	W <- nmfRes@fit@W
 	H <- nmfRes@fit@H
@@ -61,6 +72,110 @@ NMF_basis <- function(DOdata, r){
 
 	return(list(basis = W, coef = H))
 }
+
+
+NMFReconstruct <- function(year, aggType, basis_r, loggerID, new = FALSE,...){
+	erieDO <- getLakeDO(year, "B", aggType) %>% na.omit()
+	timeIdx <- index(erieDO$samplingData)
+
+	method <- list(...)$method
+	
+	methodAlias  <- method
+	if(method == "snmf/r"){
+	  methodAlias <- "snmf-r"
+	}else if(method == "snmf/l"){
+	  methodAlias <- "snmf-l"
+	}
+	
+	resFileName <- sprintf("%s/results/cluster/nmfBasis_%d_%s_%d_%s.rds",outputBaseName, year,aggType,basis_r, methodAlias)
+	if(file.exists(resFileName) & !new){
+		nmfDecomp <- readRDS(resFileName) %>% NMF_scale()
+	}else{
+		print("no previous results exist, new calculation")
+		nmfDecomp <- NMF_basis(erieDO$samplingData,basis_r,...) %>% NMF_scale()
+		saveRDS(nmfDecomp,resFileName)
+	}
+	reconstructed <- data.frame(nmfDecomp$basis %*% nmfDecomp$coef)
+	names(reconstructed) <- colnames(nmfDecomp$coef)
+	
+	res <- data.frame(nmf = reconstructed[,as.character(loggerID)], y = as.numeric(erieDO$samplingData[,loggerID]), time = timeIdx)
+	print(qplot(timeIdx, nmf, data = res) + geom_line(aes(timeIdx, y), data = res, color = "red"))
+}
+
+NMF_analysis <- function(year, aggType, basis_r = NULL,explore = FALSE, new = FALSE, ...){
+	erieDO <- getLakeDO(year, "B", aggType) %>% na.omit()
+	timeIdx <- index(erieDO$samplingData)
+	baseMap <- readRDS(sprintf("./resources/erieGoogleMap_%d.rds",year)) + labs(x = "Longitude", y = "Latitude")
+		 
+	if(is.null(basis_r)){
+	  if(year == 2015){
+	    basis_r = 5
+	  }else if(year == 2014){
+	    basis_r = 5
+	  }else if(year == 2016){
+	    basis_r = 6
+	  }
+	}
+	
+	d <- erieDO$samplingData
+	
+	require(NMF)
+	
+	method <- list(...)$method
+ 	if(is.null(method)){
+    	method <- "brunet"
+  	}
+	
+	methodAlias  <- method
+	if(method == "snmf/r"){
+		methodAlias <- "snmf-r"
+	}else if(method == "snmf/l"){
+		methodAlias <- "snmf-l"
+	}
+
+	if(explore){
+	  print("doing exploration")
+		exploreRank <- nmfEstimateRank(as.matrix(d), c(2:10), method = method)
+		saveRDS(exploreRank,sprintf("%s/results/cluster/nmfEstimateRank_%d_%s_%s.rds",outputBaseName, year,aggType, methodAlias))
+	}
+	# exploreRank <- readRDS(sprintf("%s/results/cluster/nmfEstimateRank_%d_%s.rds",outputBaseName, year,aggType))
+	
+	resFileName <- sprintf("%s/results/cluster/nmfBasis_%d_%s_%d_%s.rds",outputBaseName, year,aggType,basis_r, methodAlias)
+	if(file.exists(resFileName) & !new){
+		nmfDecomp <- readRDS(resFileName) %>% NMF_scale()
+	}else{
+		print("no previous results exist, new calculation")
+		nmfDecomp <- NMF_basis(erieDO$samplingData,basis_r,...) %>% NMF_scale()
+		saveRDS(nmfDecomp,resFileName)
+	}
+	
+	tmp <- data.frame(nmfDecomp$coef)
+	names(tmp) <- colnames(nmfDecomp$coef)
+	tmp$basis  <- 1:nrow(tmp)
+	tmp <- melt(tmp,id.vars = "basis") 
+	mergedDF <- merge(tmp,erieDO$loggerInfo,by.x = "variable", by.y = "loggerID")
+	
+	
+	# plot 
+	plot.list1 <- lapply(1:basis_r, function(r){
+		subData <- subset(mergedDF,basis == r)
+		baseMap + geom_point(aes(longitude,latitude, color = value),data = subData, size = 4) + 
+			scale_color_gradient(low = "white", high = "blue",name = "Basis\nCoefficients",limit = range(0, max(mergedDF$value)+0.1))+ 
+			ggtitle(paste0("Basis_", r)) + theme(axis.title.x = element_text(size=11),axis.title.y = element_text(size=11))
+	})
+	plot.list2 <- lapply(1:basis_r, function(r){
+		newDf <- data.frame(DO = nmfDecomp$basis[,r],time = timeIdx)
+		ggplot(data = newDf) + geom_line(aes(time, DO)) + theme_bw()
+	})
+	args.list <- c(c(plot.list1,plot.list2),list(nrow=2))
+	
+	require(gridExtra)
+	pdf(sprintf("%s/results/cluster/cluster_%d_%s_%d_%s.pdf",outputBaseName,year,aggType, basis_r, methodAlias),
+			width = 4*basis_r, height = 6)
+	print(do.call(grid.arrange, args.list))
+	dev.off()
+}
+
 
 
 SVD_basis <- function(DOdata, r){
